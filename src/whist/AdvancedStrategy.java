2b13578412949b5d06ea64d8e5e0e531a38b52e7
Tom <1599tm@gmail.com>
2019-02-05 19:57:32 +0000

    added in explanation for the empty AdvancedStrategy Class

--- src/whist/AdvancedStrategy.java
+++ src/whist/AdvancedStrategy.java
@@ -4,6 +4,12 @@ import cards.Hand;
 
 public class AdvancedStrategy implements Strategy{
 
+    /**
+     * I have not implemented this Class but have included the file to adhere to the
+     * requirements of the submission. I have still included a full description of
+     * an implementation of the Class in my AdvancedStrategyDescription.pdf
+     */
+
     @Override
     public Card chooseCard(Hand h, Trick t){
         return null;
